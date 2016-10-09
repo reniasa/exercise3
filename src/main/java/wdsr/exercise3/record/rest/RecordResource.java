97bc68a8f4f8dac74f1f57255b2c51ad8ff568ce
Renata Ciesielska <reniasac@gmail.com>
2016-10-09 15:09:29 +0200

    Resolved tests and impl to RecordResource

--- src/main/java/wdsr/exercise3/record/rest/RecordResource.java
+++ src/main/java/wdsr/exercise3/record/rest/RecordResource.java
@@ -1,11 +1,29 @@
 package wdsr.exercise3.record.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import wdsr.exercise3.record.Record;
 import wdsr.exercise3.record.RecordInventory;
 
 @Path("/records")
@@ -28,6 +46,16 @@ public class RecordResource {
 	 * ** Returns a list of all records (as application/xml)
 	 * ** Response status: HTTP 200
 	 */
+	
+	@GET
+    @Produces(MediaType.APPLICATION_XML)
+    public Response getRecords() {
+        List<Record> result = new ArrayList<>();
+        result.addAll(recordInventory.getRecords());
+        
+        GenericEntity<List<Record>> genericResult = new GenericEntity<List<Record>>(result) {};
+        return Response.ok(genericResult).build();
+    }
 
 	/**
 	 * POST https://localhost:8091/records
@@ -37,12 +65,41 @@ public class RecordResource {
 	 * ** Response status if submitted record has ID set: HTTP 400
 	 */
 	
+	@POST
+    @Consumes(MediaType.APPLICATION_XML)
+    @Produces(MediaType.APPLICATION_XML)
+    public Response createRecord(Record record, @Context UriInfo uriInfo) {
+        if(record.getId() == null) {
+            recordInventory.addRecord(record);
+        } 
+        else {
+            return Response.status(Status.BAD_REQUEST).build();
+        }
+        
+        UriBuilder builder = uriInfo.getAbsolutePathBuilder();
+        builder.path(Integer.toString(record.getId()));
+        return Response.created(builder.build()).build();
+    }
+	
 	/**
 	 * * GET https://localhost:8091/records/{id}
 	 * ** Returns an existing record (as application/xml)
 	 * ** Response status if ok: HTTP 200
 	 * ** Response status if {id} is not known: HTTP 404
 	 */
+	
+	@GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_XML)
+    public Response getRecord(@PathParam(value = "id") int id) {
+        Record record = recordInventory.getRecord(id);
+        if(record != null) {
+            return Response.ok(record).build();
+        } else { 
+            return Response.status(Status.NOT_FOUND).build();
+        }
+    }   
+	
 	 /**
 	 * * PUT https://localhost:8091/records/{id}
 	 * ** Replaces an existing record in entirety.
@@ -51,10 +108,37 @@ public class RecordResource {
 	 * ** Response status if {id} is not known: HTTP 404
 	 */
 	
+	@PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_XML)
+    @Produces(MediaType.APPLICATION_XML)
+    public Response updateRecord(Record record, @PathParam(value = "id") int id) {
+        if(record.getId() != null && id != record.getId()) {
+            return Response.status(Status.BAD_REQUEST).entity("Record ID is different in request path and message body").build();
+        }
+        record.setId(id);
+        if(recordInventory.updateRecord(id, record)) {
+            return Response.status(Status.NO_CONTENT).build();
+        } 
+        else {
+            return Response.status(Status.NOT_FOUND).build();
+        }
+    }
+	
 	/**
 	 * * DELETE https://localhost:8091/records/{id}
 	 * ** Deletes an existing record.
 	 * ** Response status if ok: HTTP 204
 	 * ** Response status if {id} is not known: HTTP 404
 	 */
+	
+	 @DELETE
+	    @Path("/{id}")
+	    public Response deleteRecord(@PathParam(value = "id") int id) {
+	        if(recordInventory.deleteRecord(id)) {
+	            return Response.status(Status.NO_CONTENT).build();
+	        } else {
+	            return Response.status(Status.NOT_FOUND).build();
+	        }
+	    }
 }
